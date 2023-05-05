23da44591d87d2d266955e48d760c0f194b6a99d
rodneydoyaoen <yendoyaoen@gmail.com>
2023-05-05 12:08:29 +0800

    Change initial layout and content of header and footer.

--- src/components/WeInspire/WeInspire.js
+++ src/components/WeInspire/WeInspire.js
@@ -1,51 +1,33 @@
-import inspireData from "@/data/inspireData";
-import bg from "@/images/backgrounds/we-inspire-bg.jpg";
-import image from "@/images/resources/we-inspire-img.jpg";
-import React, { useState } from "react";
-import { Col, Container, Image, Row } from "react-bootstrap";
+import inspireData from '@/data/inspireData';
+import bg from '@/images/backgrounds/we-inspire-bg.jpg';
+import image from '@/images/resources/we-inspire-img.jpg';
+import React, { useState } from 'react';
+import { Col, Container, Image, Row } from 'react-bootstrap';
 
 const WeInspire = () => {
   const [expand, setExpand] = useState(1);
 
   return (
     <section className="we-inspire">
-      <div
-        className="we-inspire-bg"
-        style={{ backgroundImage: `url(${bg.src})` }}
-      ></div>
+      <div className="we-inspire-bg" style={{ backgroundImage: `url(${bg.src})` }}></div>
       <div className="we-inspire-bg-overlay"></div>
       <Container>
         <Row>
           <Col xl={6} lg={6}>
             <div className="we-inspire__left">
               <div className="section-title text-left">
-                <span className="section-title__tagline">
-                  Question & Answers
-                </span>
-                <h2 className="section-title__title">
-                  We inspire and help them live better
-                </h2>
+                <span className="section-title__tagline">Question & Answers</span>
+                <h2 className="section-title__title">Everyone deserves the Right 2 Live</h2>
               </div>
               <div className="we-inspire__faq">
-                <div
-                  style={{ overflow: "hidden" }}
-                  className="accrodion-grp faq-one-accrodion"
-                >
+                <div style={{ overflow: 'hidden' }} className="accordion-grp faq-one-accordion">
                   {inspireData.map(({ id, answer, question }) => (
-                    <div
-                      className={
-                        expand === id ? "accrodion active" : "accrodion"
-                      }
-                      key={id}
-                    >
-                      <div
-                        onClick={() => setExpand(id)}
-                        className="accrodion-title"
-                      >
+                    <div className={expand === id ? 'accordion active' : 'accordion'} key={id}>
+                      <div onClick={() => setExpand(id)} className="accordion-title">
                         <h4>{question}</h4>
                       </div>
                       {expand === id && (
-                        <div className="accrodion-content animated slideInUp">
+                        <div className="accordion-content animated slideInUp">
                           <div className="inner">
                             <p>{answer}</p>
                           </div>
--- src/data/footerData.js
+++ src/data/footerData.js
@@ -1,84 +1,84 @@
-import bg from "@/images/backgrounds/footer-bg.jpg";
-import footerImage from "@/images/resources/footer-logo.jpg";
-import { contact } from "./contactData";
+import bg from '@/images/backgrounds/footer-bg.jpg';
+import footerImage from '@/images/resources/footer-logo.jpg';
+import { contact } from './contactData';
 
 const footerData = {
   ...contact,
-  link: "Halpes",
+  link: 'Halpes',
   copyrightYear: new Date().getFullYear(),
   about:
-    "Lorem ipsum dolor sit ame consect etur pisicing elit sed do eiusmod tempor incididunt ut labore.",
+    'Lorem ipsum dolor sit ame consect etur pisicing elit sed do eiusmod tempor incididunt ut labore.',
   bottomLogo: footerImage.src,
   footerBg: bg.src,
   social: [
     {
       id: 1,
-      href: "#",
-      icon: "fa-twitter"
+      href: '#',
+      icon: 'fa-twitter',
     },
     {
       id: 2,
-      href: "#",
-      icon: "fa-facebook-square"
+      href: '#',
+      icon: 'fa-facebook-square',
     },
+    // {
+    //   id: 3,
+    //   href: "#",
+    //   icon: "fa-dribbble"
+    // },
     {
       id: 3,
-      href: "#",
-      icon: "fa-dribbble"
+      href: '#',
+      icon: 'fa-instagram',
     },
-    {
-      id: 4,
-      href: "#",
-      icon: "fa-instagram"
-    }
   ],
   exploreList: [
     {
       id: 1,
-      href: "#",
-      title: "Donate"
+      href: '#',
+      title: 'Donate',
     },
     {
       id: 2,
-      href: "#",
-      title: "Campaigns"
+      href: '#',
+      title: 'Campaigns',
     },
     {
       id: 3,
-      href: "#",
-      title: "Fundraise"
+      href: '#',
+      title: 'Fundraise',
     },
     {
       id: 4,
-      href: "#",
-      title: "Volunteers"
+      href: '#',
+      title: 'Volunteers',
     },
     {
       id: 5,
-      href: "#",
-      title: "Sponsors"
+      href: '#',
+      title: 'Sponsors',
     },
     {
       id: 6,
-      href: "#",
-      title: "Fundraising"
+      href: '#',
+      title: 'Fundraising',
     },
     {
       id: 7,
-      href: "#",
-      title: "Contact"
+      href: '#',
+      title: 'Contact',
     },
     {
       id: 8,
-      href: "#",
-      title: "Help"
+      href: '#',
+      title: 'Help',
     },
     {
       id: 9,
-      href: "#",
-      title: "Faqs"
-    }
-  ]
+      href: '#',
+      title: 'Faqs',
+    },
+  ],
 };
 
 export default footerData;
