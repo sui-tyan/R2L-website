ee462828a45bc2e7ffac389d37d1a94c5944795d
elpachongco <earlsiachongco@gmail.com>
2023-05-06 02:03:49 +0800

    Update deploy configs.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,43 @@
-/**
-* @type {import('next').NextConfig}
-*/
+///**
+//* @type {import('next').NextConfig}
+//*/
+//const nextConfig = {
+//  images: {
+//    loader: "imgix",
+//    path: 'r2l.imgix.net',
+//  },
+//  assetPrefix: './',
+//};
+//
+//export default nextConfig;
+//
+///** @type {import('next').NextConfig} */
+
+/** For github pages */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
+  reactStrictMode: false,
+
+  /** For github pages */
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     loader: "imgix",
     path: 'r2l.imgix.net',
   },
-  assetPrefix: './',
+  plugins: ['autoprefixer']
 };
 
-export default nextConfig;
+module.exports = nextConfig;
