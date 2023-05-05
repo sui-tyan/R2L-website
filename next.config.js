ee5b0171819a9446306a5695f3aaa58eaf3469ee
rodneydoyaoen <yendoyaoen@gmail.com>
2023-05-05 12:06:26 +0800

    Configure imports to adjust to error.

--- next.config.js
+++ next.config.js
@@ -24,6 +24,7 @@ const nextConfig = {
     loader: "imgix",
     path: 'r2l.imgix.net',
   },
+  plugins: ['autoprefixer'],
 };
 
 module.exports = nextConfig;
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'autoprefixer',
+    // 'postcss-import',
+    // 'postcss-flexbugs-fixes',
+    // 'postcss-nested',
+    // 'postcss-custom-properties',
+  ],
+};
