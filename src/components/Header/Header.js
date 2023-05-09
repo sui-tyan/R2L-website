e917574936a3c406b17e7469cda80190749ff9a7
rodneydoyaoen <yendoyaoen@gmail.com>
2023-05-09 14:56:23 +0800

    Change header style to Header 3 and replace default logo to R2L Logo.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
-import dynamic from "next/dynamic";
-import React from "react";
-const HeaderOne = dynamic(() => import("./HeaderOne"));
-const HeaderTwo = dynamic(() => import("./HeaderTwo"));
-const HeaderThree = dynamic(() => import("./HeaderThree"));
+import dynamic from 'next/dynamic';
+import React from 'react';
+const HeaderOne = dynamic(() => import('./HeaderOne'));
+const HeaderTwo = dynamic(() => import('./HeaderTwo'));
+const HeaderThree = dynamic(() => import('./HeaderThree'));
 
 const Header = ({ pageTitle }) => {
-  return pageTitle === "Home Two" ? (
-    <HeaderTwo />
-  ) : pageTitle === "Home Three" ? (
+  return pageTitle === 'Home Three' ? (
     <HeaderThree />
+  ) : pageTitle === 'Home Two' ? (
+    <HeaderTwo />
   ) : (
     <HeaderOne />
   );
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,29 @@
-import AboutPage from "@/components/AboutPage/AboutPage";
-import BrandOne from "@/components/BrandOne/BrandOne";
-import Counters from "@/components/Counters/Counters";
-import JoinOne from "@/components/JoinOne/JoinOne";
-import Layout from "@/components/Layout/Layout";
-import PageHeader from "@/components/PageHeader/PageHeader";
-import TeamOne from "@/components/TeamOne/TeamOne";
-import TestimonialOne from "@/components/TestimonialOne/TestimonialOne";
-import React from "react";
+import AboutPage from '@/components/AboutPage/AboutPage';
+import BrandOne from '@/components/BrandOne/BrandOne';
+import Counters from '@/components/Counters/Counters';
+import JoinOne from '@/components/JoinOne/JoinOne';
+import Funding from '@/components/Funding/Funding';
+
+import Layout from '@/components/Layout/Layout';
+import PageHeader from '@/components/PageHeader/PageHeader';
+import TeamOne from '@/components/TeamOne/TeamOne';
+import TestimonialOne from '@/components/TestimonialOne/TestimonialOne';
+import React from 'react';
 
 const About = () => {
   return (
-    <Layout pageTitle="About">
-      <PageHeader pageTitle="About" />
-      <AboutPage />
-      <TestimonialOne className="about-page-testimonial" />
-      <JoinOne className="join-one__about" />
-      <Counters className="about-page-counter" />
-      <TeamOne />
-      <BrandOne />
-    </Layout>
+    <div>
+      <Funding></Funding>
+      <Layout pageTitle="Home Three">
+        <PageHeader pageTitle="About" />
+        <AboutPage />
+        <TestimonialOne className="about-page-testimonial" />
+        <JoinOne className="join-one__about" />
+        <Counters className="about-page-counter" />
+        <TeamOne />
+        <BrandOne />
+      </Layout>
+    </div>
   );
 };
 
