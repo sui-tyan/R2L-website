1dff9e52621198e7c188265455c750a944f01849
elpachongco <earlsiachongco@gmail.com>
2023-05-09 20:08:29 +0800

    Edit FAQs section.

--- src/components/WeInspire/WeInspire.js
+++ src/components/WeInspire/WeInspire.js
@@ -1,15 +1,18 @@
-import inspireData from '@/data/inspireData';
-import bg from '@/images/backgrounds/we-inspire-bg.jpg';
-import image from '@/images/resources/we-inspire-img.jpg';
-import React, { useState } from 'react';
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import inspireData from "@/data/inspireData";
+import bg from "@/images/backgrounds/we-inspire-bg.jpg";
+import image from "@/images/resources/we-inspire-img.jpg";
+import React, { useState } from "react";
+import { Col, Container, Image, Row } from "react-bootstrap";
 
 const WeInspire = () => {
   const [expand, setExpand] = useState(1);
 
   return (
     <section className="we-inspire">
-      <div className="we-inspire-bg" style={{ backgroundImage: `url(${bg.src})` }}></div>
+      <div
+        className="we-inspire-bg"
+        style={{ backgroundImage: `url(${bg.src})` }}
+      ></div>
       <div className="we-inspire-bg-overlay"></div>
       <Container>
         <Row>
@@ -20,14 +23,25 @@ const WeInspire = () => {
                   Frequently Asked Questions
                 </span>
                 <h2 className="section-title__title">
-                  Insert tagline of the company or website
+                  Information about Rights to Life
                 </h2>
               </div>
               <div className="we-inspire__faq">
-                <div style={{ overflow: 'hidden' }} className="accordion-grp faq-one-accordion">
+                <div
+                  style={{ overflow: "hidden" }}
+                  className="accordion-grp faq-one-accordion"
+                >
                   {inspireData.map(({ id, answer, question }) => (
-                    <div className={expand === id ? 'accordion active' : 'accordion'} key={id}>
-                      <div onClick={() => setExpand(id)} className="accordion-title">
+                    <div
+                      className={
+                        expand === id ? "accordion active" : "accordion"
+                      }
+                      key={id}
+                    >
+                      <div
+                        onClick={() => setExpand(id)}
+                        className="accordion-title"
+                      >
                         <h4>{question}</h4>
                       </div>
                       {expand === id && (
--- src/data/inspireData.js
+++ src/data/inspireData.js
@@ -1,19 +1,19 @@
 const inspireData = [
   {
     id: 1,
-    question: "Insert question frequently asked",
+    question: "What is Rights to Life?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
   {
     id: 2,
-    question: "Insert question frequently asked",
+    question: "What is the project about?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
   {
     id: 3,
-    question: "Insert question frequently asked",
+    question: "Who are your sponsors?",
     answer:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
   },
