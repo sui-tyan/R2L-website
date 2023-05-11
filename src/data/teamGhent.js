257aa0c851ae334a681b29b249e431a678b843e7
rodneydoyaoen <yendoyaoen@gmail.com>
2023-05-11 15:48:47 +0800

    Change image reference and order.

--- src/data/teamGhent.js
+++ src/data/teamGhent.js
@@ -1,91 +1,93 @@
 const teamGhent = [
   {
     id: 1,
-    image: "anon.png",
+    image: 'christopher-vandeviver.jpg',
     //name: "Janne",
-    title: "Christophe Vandeviver",
+    title: 'Christophe Vandeviver',
     description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
   },
   {
-    id: 1,
-    image: "anon.png",
+    id: 2,
+    image: 'tine-destrooper.jpg',
     //name: "Janne",
-    title: "Jeroen Adam",
+    title: 'Tine Destrooper',
     description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
   },
   {
-    id: 1,
-    image: "anon.png",
+    id: 3,
+    image: 'tom-beken.jpg',
     //name: "Janne",
-    title: "Marc Cools",
+    title: 'Tom Vander Beken',
     description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
   },
   {
-    id: 1,
-    image: "anon.png",
+    id: 4,
+    image: 'antoinette-verhage.jpg',
+    //name: "Janne",
+    title: 'Antoinette Verhage',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+  {
+    id: 5,
+    image: 'marc-cools.jpg',
+    //name: "Janne",
+    title: 'Marc Cools',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+  {
+    id: 6,
+    image: 'jelle-janssens.jpg',
+    //name: "Janne",
+    title: 'Jelle Janssens',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+  {
+    id: 7,
+    image: 'jeroen-adam.jpg',
+    //name: "Janne",
+    title: 'Jeroen Adam',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+  {
+    id: 8,
+    image: 'anon.png',
+    //name: "Janne",
+    title: 'Arne Vandenbogaerde',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+  {
+    id: 9,
+    image: 'noel-klima.jpg',
+    //name: "Janne",
+    title: 'Noel Klima',
+    description:
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
+  },
+
+  {
+    id: 10,
+    image: 'anon.png',
     //name: "Janne",
     title: "Kathleen D'hanis",
     description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
   },
+
   {
-    id: 1,
-    image: "anon.png",
+    id: 11,
+    image: 'elke-evrard.jpg',
     //name: "Janne",
-    title: "Tine Destrooper",
+    title: 'Elke Evrard',
     description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Elke Evrard",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Jelle Janssens",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Noel Klima",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Arne Vandenbogaerde",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Tom Vander Beken",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
-  },
-  {
-    id: 1,
-    image: "anon.png",
-    //name: "Janne",
-    title: "Antoinette Verhage",
-    description:
-      "Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.",
+      'Prof. Dr. Christophe Vandeviver is a Research Professor in Criminology (Ghent University Research Council, BOFZAP) at the Institute for International Research on Criminal Policy (IRCP), Ghent University.',
   },
 ];
 
--- src/data/teamUC.js
+++ src/data/teamUC.js
@@ -1,115 +1,101 @@
 const teamUC = [
   {
     id: 1,
-    image: "ariel-pumecha.jpg",
+    image: 'ariel-pumecha.jpg',
     //name: "Janne",
-    title: "Ariel Nimo Pumecha",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Ariel Nimo Pumecha',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 2,
-    image: "robino-cawi.jpg",
-    //name: "David",
-    title: "Robino Cawi",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    image: 'anon.png',
+    //name: "Sarah",
+    title: 'Darlyn Tagarino',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 3,
-    image: "thelma-palaoag.jpg",
-    //name: "Sarah",
-    title: "Thelma Palaoag",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    image: 'robino-cawi.jpg',
+    //name: "David",
+    title: 'Robino Cawi',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 4,
-    image: "anon.png",
+    image: 'thelma-palaoag.jpg',
     //name: "Sarah",
-    title: "Corazon Alhido-Sacpa",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Thelma Palaoag',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Cherry Ann Cabarrubias",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Corazon Alhido-Sacpa',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "John Rey Dayucos",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Cherry Ann Cabarrubias',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Leah Donato",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'John Rey Dayucos',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Chester King-eo",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Leah Donato',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Ferdinand Lawan",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Chester King-eo',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Rhodora Ngolob",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Ferdinand Lawan',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Christian Pascual",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Rhodora Ngolob',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Katrina Reyes",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Christian Pascual',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Elizabeth Sagubo",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Katrina Reyes',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
   {
     id: 5,
-    image: "anon.png",
+    image: 'anon.png',
     //name: "Sarah",
-    title: "Darly Tagarino",
-    description:
-      "There are many of lorem ipsum available but the have in some form.",
+    title: 'Elizabeth Sagubo',
+    description: 'There are many of lorem ipsum available but the have in some form.',
   },
 ];
 
