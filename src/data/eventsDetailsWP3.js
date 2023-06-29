cbcab8f28ac12335094efad95b5ead239e8795e6
elpachongco <earlsiachongco@gmail.com>
2023-06-29 22:54:26 +0800

    Add text details to work pkg 3.

--- src/data/eventsDetailsWP3.js
+++ src/data/eventsDetailsWP3.js
@@ -1,35 +1,15 @@
-import image from '@/images/resources/event-details-img-1.jpg';
+import image from "@/images/resources/event-details-img-1.jpg";
 
 const text1 =
-  'Prof. Dr. Tine Destrooper from the Faculty of Law, Department of Criminology, Criminal Law and Social Law spoke before 300 plus students on what transitional justice means and the importance of awareness and action that must be done on pending cases like that which exists in the Philippines.';
-const text2 =
-  'Hon. Jose M. Molintas, a Baguio City Counselor and Atty. Joel Dizon, a faculty member of the Faculty of Law of the University of the Cordilleras (UC), led the forum on March 9, 2023 at the UC Auditorium. They emphasized to more than 200 students that the circumstances of the past should empower change Filipinos to unify and be involved, in order to achieve true freedom and democracy for all.';
-const text3 =
-  'P/COL. Marites B. Villacarlos, Chief Of The Regional Forensic Unit-Cordillera shared the value and various roles of women in nation building, emphasizing the vast opportunity that the world may offer women despite their gender. She emphasized that women in Philippine society may not be physically equal to men but they are capable of doing more to solve things and achieve more. She ends with the line, “hindi ka babae lang (You are not a mere woman),you are more than enough.”';
-// const text4 =
-//   'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum ley of type and scrambled it to make a type specimen book.';
+  "The community outreach in Barangay Loacan, Sitio Tocmo in Itogon, Benguet went full gear as teachers of the Tocmo Integrated School together with local government officials, and University of the Cordilleras officials did a Strategic Planning on April 26, 2023. The University of the Cordilleras delegation was led by Vice President and Rights2LIFE project leader, Dr. Ariel Nimo Pumecha and the College of Criminal Justice Education Dean, Dr. Robino Cawi. The planning activity facilitated by Dr. Darlyn Tagarino, resulted in a clear identification of the targets that the community partnership will achieve in the next three years.";
 
 const eventsDetailsWP3 = [
   {
     id: 1,
     image,
-    date: '10/24/22',
-    title: 'A Lecture-Forum On Transitional Justice',
+    date: "4/26/23",
+    title: "Loakan, Tocmo, Itogon Community Outreach",
     texts: [text1],
-  },
-  {
-    id: 2,
-    image,
-    date: '5/09/23',
-    title: 'Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights',
-    texts: [text2],
-  },
-  {
-    id: 3,
-    image,
-    date: '5/16/23',
-    title: 'WE for gender equality and inclusive society": Womens’ Month Forum',
-    texts: [text3],
   },
 ];
 export default eventsDetailsWP3;
