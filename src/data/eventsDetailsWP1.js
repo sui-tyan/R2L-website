b3bfd5ac8b75ece35efa7ecbbba87318d9c6b658
arellanosophia <arellanosophiam@gmail.com>
2023-06-29 22:18:12 +0800

    Changes in Work Package 1 and 6.

--- src/data/eventsDetailsWP1.js
+++ src/data/eventsDetailsWP1.js
@@ -1,35 +1,42 @@
-import image from '@/images/resources/event-details-img-1.jpg';
+import image from "@/images/resources/event-details-img-1.jpg";
 
 const text1 =
-  'Prof. Dr. Tine Destrooper from the Faculty of Law, Department of Criminology, Criminal Law and Social Law spoke before 300 plus students on what transitional justice means and the importance of awareness and action that must be done on pending cases like that which exists in the Philippines.';
+  "Prof. Dr. Tine Destrooper from the Faculty of Law, Department of Criminology, Criminal Law and Social Law spoke before 300 plus students on what transitional justice means and the importance of awareness and action that must be done on pending cases like that which exists in the Philippines.";
 const text2 =
-  'Hon. Jose M. Molintas, a Baguio City Counselor and Atty. Joel Dizon, a faculty member of the Faculty of Law of the University of the Cordilleras (UC), led the forum on March 9, 2023 at the UC Auditorium. They emphasized to more than 200 students that the circumstances of the past should empower change Filipinos to unify and be involved, in order to achieve true freedom and democracy for all.';
+  "Hon. Jose M. Molintas, a Baguio City Counselor and Atty. Joel Dizon, a faculty member of the Faculty of Law of the University of the Cordilleras (UC), led the forum on March 9, 2023 at the UC Auditorium. They emphasized to more than 200 students that the circumstances of the past should empower change Filipinos to unify and be involved, in order to achieve true freedom and democracy for all.";
 const text3 =
-  'P/COL. Marites B. Villacarlos, Chief Of The Regional Forensic Unit-Cordillera shared the value and various roles of women in nation building, emphasizing the vast opportunity that the world may offer women despite their gender. She emphasized that women in Philippine society may not be physically equal to men but they are capable of doing more to solve things and achieve more. She ends with the line, “hindi ka babae lang (You are not a mere woman),you are more than enough.”';
-// const text4 =
-//   'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum ley of type and scrambled it to make a type specimen book.';
-
+  "P/COL. Marites B. Villacarlos, Chief Of The Regional Forensic Unit-Cordillera shared the value and various roles of women in nation building, emphasizing the vast opportunity that the world may offer women despite their gender. She emphasized that women in Philippine society may not be physically equal to men but they are capable of doing more to solve things and achieve more. She ends with the line, “hindi ka babae lang (You are not a mere woman),you are more than enough.”";
+const text4 =
+  'The University of the Cordilleras, College Student Council of the College of Business Administration (CBA), in collaboration with the College of Accountancy (COA), and College of Hospitality and Tourism Management (CHTM) held their Labor Rights Seminar "The Labor Force of Tomorrow: Be Equipped in Facing Ahead" on June 8, 2023 at UC Auditorium. One of the objectives of the seminar is to give awareness as well as to educate students on their legal rights as workers and how to utilize the said rights. The Rights2Life: Towards a Responsive Criminal Justice System in the Philippines was introduced by Dr. Darlene D. Tagarino. Mr. Rodel J. Dumlao, the 1st guest speaker, talked about Labor Standards. The second guest speaker, Mr. Rane Ramos discussed Labor Relations and the Basic Rights of Workers. Mr. Bartolome from the College of Tourism and Hospitality Management concluded with a motivational statement. The seminar is indeed a successful event. Being aware of our labor rights not only benefits the employees but also safeguards employees rights and outlines employers obligations and responsibilities. The seminar helps the graduating students understand and learn the importance of Labor Rights to be equipped for their future.';
 const eventsDetailsWP1 = [
   {
     id: 1,
     image,
-    date: '10/24/22',
-    title: 'A Lecture-Forum On Transitional Justice',
+    date: "10/24/22",
+    title: "A Lecture-Forum On Transitional Justice",
     texts: [text1],
   },
   {
     id: 2,
     image,
-    date: '5/09/23',
-    title: 'Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights',
+    date: "5/09/23",
+    title:
+      "Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights",
     texts: [text2],
   },
   {
     id: 3,
     image,
-    date: '5/16/23',
+    date: "5/16/23",
     title: 'WE for gender equality and inclusive society": Womens’ Month Forum',
     texts: [text3],
+  },
+  {
+    id: 4,
+    image,
+    date: "06/28/2023",
+    title: "Labor Rights Seminar",
+    texts: [text4],
   },
 ];
 export default eventsDetailsWP1;
--- src/data/workPackage1Details.js
+++ src/data/workPackage1Details.js
@@ -1,90 +1,90 @@
-import image2 from '@/images/resources/causes-details-images-1.jpg';
-import image3 from '@/images/resources/causes-details-images-2.jpg';
-import image from '@/images/resources/causes-details-img.jpg';
+import image2 from "@/images/resources/causes-details-images-1.jpg";
+import image3 from "@/images/resources/causes-details-images-2.jpg";
+import image from "@/images/resources/causes-details-img.jpg";
 
 const text1 =
-  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.";
+  "This will see the formulation of a new curriculum that puts together the minds of Criminal Justice education providers in the region, in the country, and from Belgium. This revised curriculum will provide maximum exposure to Criminology students on understanding the principles, the issues, and the value of Human Rights to themselves, the communities, to the nation and the world. Not only Criminology students will be targeted but all tertiary level and Sen-ior High School students will be exposed to principles of Human Rights through their general education courses.";
 const text2 =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
-
+  "The project will also aim to integrate Human Rights in the selected general education courses. Education is the best avenue to inculcate the value of Human Rights as all opportunities to exhaustively discuss issues about it, and see its value will be explored. For the duration of the project, the seeds will be planted to the students to value Human Rights as it should be valued. Education is the peaceful way to transform thinking and beliefs without resorting to force, and brings about lasting reform. Alongside the stu-dents, the teachers concerned who will be delivering the courses will be encouraged to ex-tend the knowledge and the value of respecting Human Rights to not only the students, but to their other spheres of influence as well. More importantly, through the project, UC will become a center for education, research, outreach and networking, promoting peace, justice and security in the region, and eventually in the country.";
 const summaryText =
   "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.";
 
 const workPackage1Details = {
-  raised: '25,270',
-  goal: '30,000',
-  category: 'Medical',
-  title: 'Work Packages 1',
+  raised: "25,270",
+  goal: "30,000",
+  category: "Medical",
+  title:
+    "A human-rights based education is in place: curriculum review and revision",
 
   images: [image, image2, image3],
   texts: [text1, text2],
   summaryText,
   summaryList: [
-    'Making this first true generator',
-    'Many desktop publish packages',
-    'Lorem Ipsum is not simply',
-    'If you are going to passage',
-    'It has roots in a piece',
+    "Making this first true generator",
+    "Many desktop publish packages",
+    "Lorem Ipsum is not simply",
+    "If you are going to passage",
+    "It has roots in a piece",
   ],
   comments: [
     {
       id: 1,
-      image: 'comment-1-1.jpg',
+      image: "comment-1-1.jpg",
       message:
-        'Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.',
-      sender: 'David Marks',
-      time: '3 hours ago',
+        "Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.",
+      sender: "David Marks",
+      time: "3 hours ago",
     },
     {
       id: 2,
-      image: 'comment-1-2.jpg',
+      image: "comment-1-2.jpg",
       message:
-        'Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.',
-      sender: 'Christine Eve',
-      time: '3 hours ago',
+        "Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.",
+      sender: "Christine Eve",
+      time: "3 hours ago",
     },
   ],
   sidebar: {
     activities: [
       {
         id: 1,
-        image: 'recent-donation-img-1.jpg',
+        image: "recent-donation-img-1.jpg",
 
-        amount: 'Date here',
-        name: 'A Lecture-Forum On Transitional Justice',
+        amount: "October 24, 2022",
+        name: "A Lecture-Forum On Transitional Justice",
         // time: "3 hours ago",
-        text: 'Some texts',
+        text: "Prof. Dr. Tine Destrooper from the Faculty of Law, Department of Criminology, Criminal Law and Social Law spoke... ",
       },
       {
         id: 2,
-        image: 'recent-donation-img-2.jpg',
-        amount: 'Date here',
-        name: 'Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights',
+        image: "recent-donation-img-2.jpg",
+        amount: "May 9, 2023",
+        name: "Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights",
         // time: "3 hours ago",
-        text: 'Some texts',
+        text: "Hon. Jose M. Molintas, a Baguio City Counselor and Atty. Joel Dizon a faculty member of the Faculty of...",
       },
       {
         id: 3,
-        image: 'recent-donation-img-3.jpg',
-        amount: 'Date here',
+        image: "recent-donation-img-3.jpg",
+        amount: "May 16, 2023",
         name: 'WE for gender equality and inclusive society": Womens’ Month Forum ',
         // time: "3 hours ago",
-        text: 'Some texts',
+        text: "P/COL. Marites B. Villacarlos, Chief Of The Regional Forensic Unit-Cordillera shared the...",
       },
       {
         id: 4,
-        image: 'recent-donation-img-4.jpg',
-        amount: 'Date here',
-        name: 'Event Title Here',
+        image: "recent-donation-img-4.jpg",
+        amount: "June 28, 2023",
+        name: "Labor Rights Seminar",
         // time: "3 hours ago",
-        text: 'Some texts',
+        text: "One of the objectives of the seminar is to give awareness as well as to educate students on their...",
       },
     ],
     lead: {
       image: image,
-      name: 'Insert name here',
-      education: 'education',
-      location: 'location',
+      name: "Dr. Robino D. Cawi",
+      education: "CCJE",
+      location: "University of the Cordilleras",
     },
   },
 };
