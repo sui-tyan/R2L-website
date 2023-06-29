3418a81cd14d53138d88b6807e3c7da521880ed3
elpachongco <earlsiachongco@gmail.com>
2023-06-29 22:48:52 +0800

    Add text details to workpackage 4.

--- src/data/eventsDetailsWP4.js
+++ src/data/eventsDetailsWP4.js
@@ -1,35 +1,25 @@
-import image from '@/images/resources/event-details-img-1.jpg';
+import image from "@/images/resources/event-details-img-1.jpg";
 
 const text1 =
-  'Prof. Dr. Tine Destrooper from the Faculty of Law, Department of Criminology, Criminal Law and Social Law spoke before 300 plus students on what transitional justice means and the importance of awareness and action that must be done on pending cases like that which exists in the Philippines.';
+  "A series of training on Criminological Research Design Formulation in partnership with Ghent University will be conducted sometime in June and July, 2023. Some slots will be open to the public. More information about the training will be uploaded by the first week of June, 2023.";
 const text2 =
-  'Hon. Jose M. Molintas, a Baguio City Counselor and Atty. Joel Dizon, a faculty member of the Faculty of Law of the University of the Cordilleras (UC), led the forum on March 9, 2023 at the UC Auditorium. They emphasized to more than 200 students that the circumstances of the past should empower change Filipinos to unify and be involved, in order to achieve true freedom and democracy for all.';
-const text3 =
-  'P/COL. Marites B. Villacarlos, Chief Of The Regional Forensic Unit-Cordillera shared the value and various roles of women in nation building, emphasizing the vast opportunity that the world may offer women despite their gender. She emphasized that women in Philippine society may not be physically equal to men but they are capable of doing more to solve things and achieve more. She ends with the line, “hindi ka babae lang (You are not a mere woman),you are more than enough.”';
-// const text4 =
-//   'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum ley of type and scrambled it to make a type specimen book.';
+  "On November 23, 2023, Prof Tine Destrooper shared her experience to about 20 faculty members of the College of Criminal Criminal Justice and the College of Arts and Sciences of the University of the Cordilleras who were receptive to ways in which various courses with aspects of human rights may be better taught.";
 
 const eventsDetailsWP4 = [
   {
     id: 1,
     image,
-    date: '10/24/22',
-    title: 'A Lecture-Forum On Transitional Justice',
+    date: "6/1/23",
+    title: "Criminological Research Design Training",
     texts: [text1],
   },
   {
     id: 2,
     image,
-    date: '5/09/23',
-    title: 'Remembering EDSA: Implications of the EDSA Revolution to Political and Civil Rights',
+    date: "10/23/22",
+    title:
+      "Lecture-Workshop on Learning Resources and Delivery Techniques of Human Rights Courses",
     texts: [text2],
-  },
-  {
-    id: 3,
-    image,
-    date: '5/16/23',
-    title: 'WE for gender equality and inclusive society": Womens’ Month Forum',
-    texts: [text3],
   },
 ];
 export default eventsDetailsWP4;
--- src/data/workPackage4Details.js
+++ src/data/workPackage4Details.js
@@ -1,90 +1,73 @@
-import image2 from '@/images/resources/causes-details-images-1.jpg';
-import image3 from '@/images/resources/causes-details-images-2.jpg';
-import image from '@/images/resources/causes-details-img.jpg';
-import thelmaPalaoag from '@/images/team/thelma-palaoag.jpg';
+import image2 from "@/images/resources/causes-details-images-1.jpg";
+import image3 from "@/images/resources/causes-details-images-2.jpg";
+import image from "@/images/resources/causes-details-img.jpg";
+import cherryCabarrubias from "@/images/team/anon.png";
 
 const text1 =
-  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text.";
-const text2 =
-  "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  "The competencies and moral fiber of individuals who play key roles in promoting the rule of law and human rights will be developed.";
 
 const summaryText =
-  "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing.";
+  "The focus will be the students, the faculty members, and then the  criminal justice movers like the police. Competence will be developed through education, research and outreach involvement. Continuing education will be part of the outreach activities.";
 
 const workPackage4Details = {
-  raised: '25,270',
-  goal: '30,000',
-  category: 'Medical',
-  title: 'Strengthened Research and Development attuned to the needs of the times',
+  raised: "25,270",
+  goal: "30,000",
+  category: "Medical",
+  title:
+    "Competent criminal justice movers among the faculty members, students and the community",
 
   images: [image, image2, image3],
-  texts: [text1, text2],
+  texts: [text1],
   summaryText,
   summaryList: [
-    'Making this first true generator',
-    'Many desktop publish packages',
-    'Lorem Ipsum is not simply',
-    'If you are going to passage',
-    'It has roots in a piece',
+    "Making this first true generator",
+    "Many desktop publish packages",
+    "Lorem Ipsum is not simply",
+    "If you are going to passage",
+    "It has roots in a piece",
   ],
   comments: [
     {
       id: 1,
-      image: 'comment-1-1.jpg',
+      image: "comment-1-1.jpg",
       message:
-        'Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.',
-      sender: 'David Marks',
-      time: '3 hours ago',
+        "Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.",
+      sender: "David Marks",
+      time: "3 hours ago",
     },
     {
       id: 2,
-      image: 'comment-1-2.jpg',
+      image: "comment-1-2.jpg",
       message:
-        'Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.',
-      sender: 'Christine Eve',
-      time: '3 hours ago',
+        "Sending love. My nephews Nick and Anthony Salaber are your teammates, so I know the caliber person you are. Our whole family is sending our best to you and your family.",
+      sender: "Christine Eve",
+      time: "3 hours ago",
     },
   ],
   sidebar: {
     activities: [
       {
         id: 1,
-        image: 'recent-donation-img-1.jpg',
-        amount: 'Date here',
-        name: 'Event Title Here',
+        image: "recent-donation-img-1.jpg",
+        amount: "June 1, 2023",
+        name: "Criminological Research Design Training",
         // time: "3 hours ago",
-        text: 'Some texts',
+        text: "A series of training on Criminological Research Design Formulation...",
       },
       {
         id: 2,
-        image: 'recent-donation-img-2.jpg',
-        amount: 'Date here',
-        name: 'Event Title Here',
+        image: "recent-donation-img-2.jpg",
+        amount: "October 23, 2022",
+        name: "Lecture-Workshop on Learning Resources and Delivery Techniques of Human Rights Courses",
         // time: "3 hours ago",
-        text: 'Some texts',
-      },
-      {
-        id: 3,
-        image: 'recent-donation-img-3.jpg',
-        amount: 'Date here',
-        name: 'Event Title Here',
-        // time: "3 hours ago",
-        text: 'Some texts',
-      },
-      {
-        id: 4,
-        image: 'recent-donation-img-4.jpg',
-        amount: 'Date here',
-        name: 'Event Title Here',
-        // time: "3 hours ago",
-        text: 'Some texts',
+        text: "Prof Tine Destrooper shared her experience to about 20 faculty...",
       },
     ],
     lead: {
-      image: thelmaPalaoag,
-      name: 'Dr. Thelma D. Palaoag',
-      education: 'CITCS',
-      location: 'University of the Cordilleras. Governor Pack Road, Baguio City 2600, Philippines',
+      image: cherryCabarrubias,
+      name: "Cherry Ann A. Cabarrubias",
+      education: "CCJE",
+      location: "University of the Cordilleras",
     },
   },
 };
