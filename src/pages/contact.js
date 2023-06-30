a695f16a918df61514543f39eb60b06326c019a0
elpachongco <79735546+elpachongco@users.noreply.github.com>
2023-06-30 12:06:11 +0800

    Edit contact us tab text.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,16 @@
-import BecomeVolunteer from '@/components/BecomeVolunteer/BecomeVolunteer';
-import ContactPage from '@/components/ContactPage/ContactPage';
-import GoogleMap from '@/components/GoogleMap/GoogleMap';
-import Funding from '@/components/Funding/Funding';
-import Layout from '@/components/Layout/Layout';
-import PageHeader from '@/components/PageHeader/PageHeader';
-import React from 'react';
-import bg from '@/images/backgrounds/contactus-bg.jpg';
+import BecomeVolunteer from "@/components/BecomeVolunteer/BecomeVolunteer";
+import ContactPage from "@/components/ContactPage/ContactPage";
+import GoogleMap from "@/components/GoogleMap/GoogleMap";
+import Funding from "@/components/Funding/Funding";
+import Layout from "@/components/Layout/Layout";
+import PageHeader from "@/components/PageHeader/PageHeader";
+import React from "react";
+import bg from "@/images/backgrounds/contactus-bg.jpg";
 
 const Contact = () => {
   return (
     <div>
-      <Layout pageTitle="Home Three">
+      <Layout pageTitle="Contact Us">
         <PageHeader header={bg} pageTitle="Contact" />
         <ContactPage />
         {/* <GoogleMap /> */}
