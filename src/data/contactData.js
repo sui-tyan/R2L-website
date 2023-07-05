751b0346f38c721d3508232fa7168665b452a4d9
arellanosophia <arellanosophiam@gmail.com>
2023-07-05 12:51:17 +0800

    Add details in Contact Page.

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -1,16 +1,16 @@
 import image from "@/images/resources/contact-page-img-1.jpg";
 
 export const contact = {
-  tel: "92 666 888 0000",
-  email: "r2l@gmail.com",
-  officeAddress: "Legarda Rd, Baguio, 2600 Benguet",
+  tel: "(074) 442-3316 loc. no. 309",
+  email: "vliruos@uc-bcf.edu.ph",
+  officeAddress: "UC Legarda Campus, Baguio City",
 };
 
 const contactData = {
   ...contact,
   image,
   description:
-  'Rights2LIFE: Towards a Responsive Criminal Justice System in the Philippines',
+    "Rights2LIFE: Towards a Responsive Criminal Justice System in the Philippines",
 };
 
 export default contactData;
