4f0789e0fdbc3526bf2e3bde95789790665ca834
arellanosophia <arellanosophiam@gmail.com>
2023-07-08 23:48:56 +0800

    Remove some comments in Meet the Team Page.

--- src/data/teamUC.js
+++ src/data/teamUC.js
@@ -1,53 +1,53 @@
 const teamUC = [
   {
     id: 1,
-    image: 'ariel-pumecha.jpg',
-    //name: "Janne",
-    title: 'Dr. Ariel Nimo Pumecha',
-    description: 'Dr. Ariel is the Vice President for Academics and Research at University of the Cordilleras. He is the partner promoter.',
+    image: "ariel-pumecha.jpg",
+    title: "Dr. Ariel Nimo Pumecha",
+    description:
+      "Dr. Ariel is the Vice President for Academics and Research at University of the Cordilleras. He is the partner promoter.",
   },
   {
     id: 2,
-    image: 'darlyn-tagarino.jpg',
-    //name: "Sarah",
-    title: 'Dr. Darlyn Tagarino',
-    description: 'Dr. Darlyn is a faculty of Criminal Justice Education at University of the Cordilleras. She is the Lead in Domain 5-A',
+    image: "darlyn-tagarino.jpg",
+    title: "Dr. Darlyn Tagarino",
+    description:
+      "Dr. Darlyn is a faculty of Criminal Justice Education at University of the Cordilleras. She is the Lead in Domain 5-A",
   },
   {
     id: 3,
-    image: 'robino-cawi.jpg',
-    //name: "David",
-    title: 'Dr. Robino Cawi',
-    description: 'Dr. Robino is the dean of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 1-A',
+    image: "robino-cawi.jpg",
+    title: "Dr. Robino Cawi",
+    description:
+      "Dr. Robino is the dean of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 1-A",
   },
   {
     id: 4,
-    image: 'thelma-palaoag.jpg',
-    //name: "Sarah",
-    title: 'Dr. Thelma Palaoag',
-    description: 'Dr. Thelma is a faculty of Information Technology and Computer Science at University of the Cordilleras. She is the Lead in Domain 2-A',
+    image: "thelma-palaoag.jpg",
+    title: "Dr. Thelma Palaoag",
+    description:
+      "Dr. Thelma is a faculty of Information Technology and Computer Science at University of the Cordilleras. She is the Lead in Domain 2-A",
   },
   {
     id: 5,
-    image: 'scyld-segundo.jpg',
-    //name: "Sarah",
-    title: 'Sclyd Segundo',
-    description: 'Sclyd is a faculty of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 3.',
+    image: "scyld-segundo.jpg",
+    title: "Sclyd Segundo",
+    description:
+      "Sclyd is a faculty of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 3.",
   },
   {
     id: 6,
-    image: 'anon.png',
-    //name: "Sarah",
-    title: 'Cherry Ann Cabarrubias',
-    description: 'Cherry is a faculty of Criminal Justice Education at University of the Cordilleras. She is the Lead in Domain 4',
+    image: "anon.png",
+    title: "Cherry Ann Cabarrubias",
+    description:
+      "Cherry is a faculty of Criminal Justice Education at University of the Cordilleras. She is the Lead in Domain 4",
   },
   {
     id: 7,
-    image: 'anon.png',
-    //name: "Sarah",
-    title: 'Chester King-eo',
-    description: 'Chester is a faculty of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 6',
-  }
+    image: "anon.png",
+    title: "Chester King-eo",
+    description:
+      "Chester is a faculty of Criminal Justice Education at University of the Cordilleras. He is the Lead in Domain 6",
+  },
 ];
 
 export default teamUC;
