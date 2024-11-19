b6c4f6573946bef21ccb3bd324a7c9d6050739b7
sui-tyan <mallari23adrian@gmail.com>
2024-11-19 19:39:27 +0800

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -16,31 +16,32 @@
 /** For github pages */
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-let assetPrefix = "";
-let basePath = "";
+let assetPrefix = '';
+let basePath = '';
 
 if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
 
   // Disabled for application of subdomain rights2life.uc-bcf.edu.ph
   // enable if using github.io domain.
-  
+
   //assetPrefix = `/${repo}/`;
   //basePath = `/${repo}`;
 }
 
 const nextConfig = {
+  output: 'export',
   reactStrictMode: false,
 
   /** For github pages */
   basePath: basePath,
   assetPrefix: assetPrefix,
   images: {
-    loader: "imgix",
+    loader: 'imgix',
     path: 'r2l.imgix.net',
   },
-  plugins: ['autoprefixer']
+  plugins: ['autoprefixer'],
 };
 
 export default nextConfig;
